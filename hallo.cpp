f7461e45134a825567052910e45408ea2433d5d1
Ahmad Alifariz Anwar <hanaaraashi@gmail.com>
2025-05-23 15:38:50 +0800

    Menampilkan data pada array

--- hallo.cpp
+++ hallo.cpp
@@ -4,6 +4,10 @@ using namespace std;
 void Names ()
 {
     string Nama [5] = {"Liu","Cakra","Suisei","Gura","Archer"};
+
+    for (int i = 0 ; i<5 ; i++) {
+       cout << Nama [i] << "\n"; 
+    }
 }
 
 int main() {
