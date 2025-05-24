82b430525aec8451849776bfb26f16773fdcd74a
Ahmad Alifariz Anwar <hanaaraashi@gmail.com>
2025-05-24 15:11:41 +0800

    Memodifikasi program agar menerima masukkan jumlah nama yang diproses

--- hallo.cpp
+++ hallo.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
 using namespace std;
 
-void Names ()
+void names ()
 {
-    string Nama [5] = {"Liu","Cakra","Suisei","Gura","Archer"};
-
-    for (int i = 0 ; i<5 ; i++) {
-       cout << Nama [i] << "\n"; 
+    string nama [5];
+    // Input nama
+    for (int i = 0; i<5 ; i++) {
+        cout << "Masukkan nama ke-" << i + 1 << ":";
+        cin >> nama [i];
     }
+
+    //Menampilkan nama
+    cout << "\n+-------------------+\n";
+    cout << "|       Daftar      |\n";
+    cout << "|        Nama       |\n";
+    cout << "+-------------------+\n";
+    
+    for (int i = 0 ; i<5 ; i++) {
+        cout << "Nama ke-" << i + 1 << ":" << nama [i] << "\n";
+    }
+
 }
 
+
 int main() {
-    cout << "Hello World!\n";
-    cout << "Indonesia Yok Hebat\n";
+    cout << "==============================\n";
+    cout << "         Hello World!\n";
+    cout << "     Indonesia Yok Hebat\n";
+    cout << "==============================\n\n";
 
-    Names();
+    cout << "Input Penduduk Indonesia\n\n";
+    names();
 
+    
     return 0;
 }
